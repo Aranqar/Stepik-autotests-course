d9d0c30490463951f04e17902da782b7081a7587
Recklesst <Happydaysknow@gmail.com>
2021-07-16 18:02:08 +1000

    Setup func about new user registration and tests for that user

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,12 +9,29 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "/login/1" in self.url, "Not found login_url"
-
+        assert "/login/" in self.url, "Not found login_url"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
-
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def should_be_all_registration_fields(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "Registration email field is not " \
+                                                                               "presented "
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD), "Registration password field is not" \
+                                                                                  " presented "
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTRATION_PASS_AGAIN), "Registration pass again is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_BUTTON), "Registration button is not presented"
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        email_field.send_keys(email)
+        password_field1 = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD)
+        password_field2 = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASS_AGAIN)
+        password_field1.send_keys(password)
+        password_field2.send_keys(password)
+        reg_button = self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON)
+        reg_button.click()
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,41 @@ import time
 from pages.product_page import ProductPage
 import pytest
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+import faker
+
+
+@pytest.mark.new
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        page.should_be_all_registration_fields()
+        f = faker.Faker()
+        email = f.email()
+        password = str(time.time())
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.prices_should_be_same()
+        page.added_product_names_should_be_same()
+
 
 xfile = 7
 product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
@@ -12,13 +47,14 @@ links.insert(xfile, xlink)
 
 @pytest.mark.parametrize('promo_offers', links)
 def test_guest_can_add_product_to_basket(browser, promo_offers):
-     page = ProductPage(browser, promo_offers)
-     page.open()
-     page.should_not_be_success_message()
-     page.add_product_to_basket()
-     page.solve_quiz_and_get_code()
-     page.prices_should_be_same()
-     page.added_product_names_should_be_same()
+    page = ProductPage(browser, promo_offers)
+    page.open()
+    page.should_not_be_success_message()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.prices_should_be_same()
+    page.added_product_names_should_be_same()
+
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -28,11 +64,13 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.should_not_be_success_message()
 
+
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
@@ -42,11 +80,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.success_message_should_not_dissapeared()
 
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -54,7 +94,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.new
+
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -63,5 +103,3 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_no_products_in_basket()
     basket_page.should_be_message_basket_is_empty()
-
-
