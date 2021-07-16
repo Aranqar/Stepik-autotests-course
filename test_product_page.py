f685b8bbfb973cda39b533a8e2859bb2d02dbb1d
Recklesst <Happydaysknow@gmail.com>
2021-07-16 18:18:17 +1000

    Final preparations

--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,6 @@ from pages.login_page import LoginPage
 import faker
 
 
-@pytest.mark.new
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -27,6 +26,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
@@ -40,12 +40,13 @@ class TestUserAddToBasketFromProductPage:
 
 xfile = 7
 product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
-links = [product_base_link + str(i) for i in range(1) if i != xfile]
+links = [product_base_link + str(i) for i in range(10) if i != xfile]
 xlink = pytest.param(product_base_link + str(xfile), marks=pytest.mark.xfail(reason="Error on page"))
-links.insert(xfile, xlink)
+links.insert(xfile, xlink) # Создаем список офферов и помечаем один как заведомо упавший
 
 
 @pytest.mark.parametrize('promo_offers', links)
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, promo_offers):
     page = ProductPage(browser, promo_offers)
     page.open()
@@ -88,6 +89,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -95,6 +97,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
